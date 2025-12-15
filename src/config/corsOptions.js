2b589917f02365e0c314ea2776073b8b2eb82b50
Anshu Pandey <anshuies31@gmail.com>
2025-12-16 00:42:33 +0530

    fixed

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,7 +1,22 @@
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "https://recruitment-client-anshu-pandeys-projects.vercel.app",
+  "https://recruitment-client-git-dev-anshu-pandeys-projects.vercel.app"
+];
 
 export const corsOptions = {
-  origin: true,
+  origin: (origin, callback) => {
+    // allow server-to-server / Postman
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error("Not allowed by CORS"));
+  },
   credentials: true,
-  optionsSuccessStatus: 200,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
