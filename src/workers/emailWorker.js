f8f82ea3b4daf66ad24f6370412ae9da35cf8f85
Amit Kumar Patel <amitpatel9302352967@gmail.com>
2025-12-23 16:17:32 +0530

    added new feature for forgot-password

--- src/workers/emailWorker.js
+++ src/workers/emailWorker.js
@@ -2,7 +2,7 @@
 import { Worker } from 'bullmq';
 import connection from '../config/config/bullmq-connection.js';
 import logger from '../utils/logger.js';
-import { sendEnrollEmail, sendVerificationEmail, sendWelcomeEmail } from '../services/sendMail.js';
+import { sendEnrollEmail, sendVerificationEmail, sendWelcomeEmail,sendResetPasswordEmail } from '../services/sendMail.js';
 
 // NO QueueScheduler needed anymore in BullMQ v5+
 // BullMQ auto-handles delayed jobs, retries, etc. when Worker starts
@@ -20,8 +20,9 @@ const worker = new Worker(
       }
       else if (job.name === 'enroll-candidate') {
         await sendEnrollEmail(job.data)
-      }
-      else {
+      }else if (job.name === "reset-password") {
+        await sendResetPasswordEmail(job.data)
+      }else {
         logger.warn(`Unknown job type: ${job.name}`);
       }
     } catch (error) {
