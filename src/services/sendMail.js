d9f4d3d471ebca3b3f6b2475ae01d77f5761a854
ikartikeyo <kartikeypandey0011@gmail.com>
2025-12-24 18:00:59 +0530

    send Interviewer mail interface change

--- src/services/sendMail.js
+++ src/services/sendMail.js
@@ -90,22 +90,29 @@ export async function sendInterviewerEmail(data) {
     
     const payload = {
       sender: { name: "Sheryians", email: "anshur9608837@gmail.com" },
-      to: [{ email: data.interviewer, name: data.candidateName}],
+      to: [{ email: data.interviewer}],
       subject: `Interview Scheduled for ${data.jobTitle}`,
       htmlContent: `
         <div style="font-family: Arial; padding: 20px; background: #f4f4f4; border-radius: 10px;">
-          <h1 style="color: #1a73e8;">Hi ${data.candidateName || "Candidate"}!</h1>
-          <p>Your interview for <strong>${data.jobTitle}</strong> at <strong>Sheryians</strong> has been scheduled.</p>
+          <h1 style="color: #1a73e8;">Dear interviewer,</h1>
+          <p>I hope this email finds you well.</p>
+          <p>This is to inform you that an interview has been scheduled for the following candidate:</p>
+          <p><strong>Candidate Name:</strong> ${data.candidateName}</p>
+          <p><strong>Position:</strong> ${data.jobTitle}</p>
           <p><strong>Date & Time:</strong> ${new Date(data.Timing).toLocaleString()}</p>
           <p><strong>Meeting Link:</strong></p>
           <a href="${data.meetingLink}" target="_blank">${data.meetingLink}</a>
           <br /><br />
-          <p>Please join on time and ensure a stable internet connection.</p>
+
+          <p>Kindly let us know if the scheduled time works for you or if any adjustments are required.</p>
+          <p>Please feel free to reach out if you have any questions or need further information.</p>
+          <p>Thank you for your time and cooperation.</p>
           <hr />
-          <small>Best of luck!</small>
+          <small>Best regards, Sheryians Team</small>
+          
         </div>
       `,
-      textContent: `Hi ${data.name}, your interview for ${data.jobTitle} is scheduled on ${new Date(data.interviewTime).toLocaleString()}. Meeting link: ${data.meetingLink}`,
+      textContent: `Dear Interviwer, I hope this email finds you well. This is to inform you that an interview has been scheduled for the following candidate Name: ${data.name} Position: ${data.jobTitle} Date & Time: ${new Date(data.interviewTime).toLocaleString()}. Meeting link: ${data.meetingLink}`,
     };
     console.log(payload)
     const response = await axios.post(BREVO_URL, payload, {
