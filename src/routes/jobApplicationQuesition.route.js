401b6271c757f8c001fe1ef9ccbc13b2e3baf1f8
Ayush Ahirwar <ayushahirwar04@gmail.com>
2026-01-09 17:41:44 +0530

    added validators

--- src/routes/jobApplicationQuesition.route.js
+++ src/routes/jobApplicationQuesition.route.js
@@ -1,16 +1,32 @@
 import express from "express";
 import { authenticateJWT } from "../middlewares/auth.middleware.js";
 import { authorize } from "../middlewares/role.middleware.js";
-import jobApplicationQuestionController from "../controllers/jobApplicatonQuestion.controller.js"
+import jobApplicationQuestionController from "../controllers/jobApplicatonQuestion.controller.js";
+import { createQuestionsValidator, updateQuestionValidator } from "../middlewares/validators/jobApplicationQuestion.validator.js";
 
 const router = express.Router();
 
+router.post(
+  "/createjobquestions/:id",
+  authenticateJWT,
+  authorize("admin"),
+  createQuestionsValidator,
+  jobApplicationQuestionController.createApplicationQuestion
+);
 
-router.post("/createjobquestions/:id",authenticateJWT,authorize("admin"),jobApplicationQuestionController.createApplicationQuestion);
+router.get(
+  "/getjobquestions/:id",
+  authenticateJWT,
+  authorize("admin"),
+  jobApplicationQuestionController.getApplicationQuestion
+);
 
-router.get("/getjobquestions/:id",authenticateJWT,authorize("admin"),jobApplicationQuestionController.getApplicationQuestion);
-
-router.post("/updatejobquestion/:id",authenticateJWT,authorize("admin"),jobApplicationQuestionController.updateApplicationQuestion)
-
+router.post(
+  "/updatejobquestion/:id",
+  authenticateJWT,
+  authorize("admin"),
+  updateQuestionValidator,
+  jobApplicationQuestionController.updateApplicationQuestion
+);
 
 export default router;
