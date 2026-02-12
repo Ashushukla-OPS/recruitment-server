0df5988d3be8889f87925ac7bcea3f388ac0c33d
dwivedisarthak82 <srkdwivedi82@gmail.com>
2026-02-12 17:34:44 +0530

    blogpost backend final Api

--- src/repositories/contracts/IBlogPostRepository.js
+++ src/repositories/contracts/IBlogPostRepository.js
@@ -1,19 +1,35 @@
 class BlogPostRepository {
-  async create(data) {}
+  async create(data) {
+    throw new Error('Method not implemented.');
+  }
 
-  async findPaginated(filter, skip, limit) {}
+  async findPaginated(filter, skip, limit) {
+throw new Error('Method not implemented.');
+  }
 
-  async searchBlogs(filters, options) {}
+  async searchBlogs(filters, options) {
+    throw new Error('Method not implemented.');
+  }
 
-  async count(filter) {}
+  async count(filter) {
+throw new Error('Method not implemented.');
+  }
 
-  async findById(id) {}
+  async findById(id) {
+throw new Error('Method not implemented.');
+  }
 
-  async findBySlug(slug) {}
+  async findBySlug(slug) {
+throw new Error('Method not implemented.');
+  }
 
-  async updateById(id, data) {}
+  async updateById(id, data) {
+throw new Error('Method not implemented.');
+  }
 
-  async deleteById(id) {}
+  async deleteById(id) {
+throw new Error('Method not implemented.');
+  }
 }
 
 export default BlogPostRepository;
